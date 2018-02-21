32eaf27c0fd44bc83c13925b27b14bf920d9f49d
Kiven-hu <30894710+Kiven-hu@users.noreply.github.com>
2018-02-22 01:11:40 +0800

    Day17

---
+++ Day16&Day17/Day16(1)/Day16(1)/Day16(1).cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class wheel;
+class car{
+public:
+	car(char *pn);
+	void run(wheel &w);
+private:
+	char name[20];
+
+};
+car::car(char *pn)
+{
+	strcpy(name, pn);
+}
+
+class wheel{
+public:
+	wheel(int s);
+	friend void car::run(wheel &w);
+private:
+	int speed;
+};
+wheel::wheel(int s)
+{
+	speed = s;
+}
+void car::run(wheel &w)
+{
+	cout << "the car is running" << endl;
+	cout << "name:" << name << "speed:" << w.speed << endl;
+}
+int main()
+{
+	wheel w(60);
+	car c("New car");
+	c.run(w);
+	return 0;
+}
+
